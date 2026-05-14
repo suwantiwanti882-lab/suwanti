d736c15c47cade24cd0d772be6312bd8bf1aa3df
Suwanti <suwantiwanti882@gmail.com>
2026-05-14 13:47:50 +0700

    update

--- stok.php
+++ stok.php
@@ -316,25 +316,6 @@ if (isset($_POST['submit'])) {
 
       </div>
     </section>
-
-    <div class="card">
-      <div class="card-body">
-        <h5 class="card-title">Example Card</h5>
-        <p>This is an examle page with no contrnt. You can use it as a starter for your custom pages.</p>
-      </div>
-    </div>
-
-    </div>
-
-    <div class="col-lg-6">
-
-      <div class="card">
-        <div class="card-body">
-          <h5 class="card-title">Example Card</h5>
-          <p>This is an examle page with no contrnt. You can use it as a starter for your custom pages.</p>
-        </div>
-      </div>
-
     </div>
     </div>
     </section>
