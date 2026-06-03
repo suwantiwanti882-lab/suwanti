7e54626ab5fe6a9a1aaead8be77eb06d895ed6b2
Suwanti <suwantiwanti882@gmail.com>
2026-06-03 10:37:25 +0700

    update

--- e_produk.php
+++ e_produk.php
@@ -224,7 +224,7 @@ if (isset($_POST['update'])) {
 
                     <div class="card">
                         <div class="card-body">
-                            <h5 class="card-title">Tambah Produk</h5>
+                            <h5 class="card-title">Edit Produk</h5>
 
                             <form class="row g-3" method="post" enctype="multipart/form-data">
                                 <div class="col-12">
